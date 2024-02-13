c556404aa5fde464e40b3ffcdc054645c4e0ac42
tohir babatunde <tohirbabs@gmail.com>
2024-02-13 13:21:17 +0100

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,12 +10,12 @@ return (
         <img src="/titlebar.png" alt="" className="border-b " />
         <div className="flex flex-col relative ">
             <img src="/harvestgrove.png" alt="" className="object-cover w-full h-full" />      
-            {/* <img src="/sect2.png" alt="" className="object-cover w-full h-full" />     
+            <img src="/sect2.png" alt="" className="object-cover w-full h-full" />     
                    <img src="/sect3.png" alt="" className="object-cover w-full h-full" />
             <img src="/sect4.png" alt="" className="object-cover w-full h-full" />
             <img src="/sect5.png" alt="" className="object-cover w-full h-full" />
 
-      <img src="/footer.png" alt="" className="object-cover w-full h-full" /> */}
+      <img src="/footer.png" alt="" className="object-cover w-full h-full" />
 
             <a href="" className="absolute bottom-4 right-4 text-xs bg-white px-2 shadow border w-max p-1  rounded-md flex items-center gap-1"><span>harvestgrove</span>
           <HiOutlineArrowUpRight/>
@@ -34,12 +34,7 @@ return (
         <img src="/titlebar.png" alt="" className="border-b " />
         <div className="flex flex-col relative ">
             <img src="/galaria.png" alt="" className="object-cover w-full h-full" />      
-            {/* <img src="/sect2.png" alt="" className="object-cover w-full h-full" />     
-                   <img src="/sect3.png" alt="" className="object-cover w-full h-full" />
-            <img src="/sect4.png" alt="" className="object-cover w-full h-full" />
-            <img src="/sect5.png" alt="" className="object-cover w-full h-full" />
-
-      <img src="/footer.png" alt="" className="object-cover w-full h-full" /> */}
+            <img src="/galaria2.png" alt="" className="object-cover w-full h-full" />
 
             <a href="" className="absolute bottom-4 right-4 text-xs bg-white px-2 shadow border w-max p-1  rounded-md flex items-center gap-1"><span>harvestgrove</span>
           <HiOutlineArrowUpRight/>
