4b0a18bd830de4d4f2d8e34fa1eb81f163c8029e
tohir babatunde <tohirbabs@gmail.com>
2024-02-13 13:49:22 +0100

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,16 +6,16 @@ import { Weather } from "./widgets/Weather"
 function Projects (){
 return (
     <div className="lg:max-w-2xl p-2 flex flex-col gap-10">
-        <div className="flex flex-col  rounded-lg rounded-b-2xl border bg-white shadow overflow-hidden">
+        <div className="flex flex-col rounded-lg rounded-b-2xl border-4 border-slate-800 bg-white shadow overflow-hidden">
         <img src="/titlebar.png" alt="" className="border-b " />
         <div className="flex flex-col relative ">
             <img src="/harvestgrove.png" alt="" className="object-cover w-full h-full" />      
-            <img src="/sect2.png" alt="" className="object-cover w-full h-full" />     
-                   <img src="/sect3.png" alt="" className="object-cover w-full h-full" />
-            <img src="/sect4.png" alt="" className="object-cover w-full h-full" />
-            <img src="/sect5.png" alt="" className="object-cover w-full h-full" />
+            {/* <img src="/sect2.png" alt="" className="object-cover w-full h-full" />      */}
+                   {/* <img src="/sect3.png" alt="" className="object-cover w-full h-full" /> */}
+            {/* <img src="/sect4.png" alt="" className="object-cover w-full h-full" /> */}
+            {/* <img src="/sect5.png" alt="" className="object-cover w-full h-full" /> */}
 
-      <img src="/footer.png" alt="" className="object-cover w-full h-full" />
+      {/* <img src="/footer.png" alt="" className="object-cover w-full h-full" /> */}
 
             <a href="" className="absolute bottom-4 right-4 text-xs bg-white px-2 shadow border w-max p-1  rounded-md flex items-center gap-1"><span>harvestgrove</span>
           <HiOutlineArrowUpRight/>
@@ -30,11 +30,11 @@ return (
                 <Weather/>
 
             </div></div>  
-            <div className="flex flex-col  rounded-lg rounded-b-2xl border bg-white shadow overflow-hidden">
+            <div className="flex flex-col  rounded-lg rounded-b-2xl border-4 bg-white shadow overflow-hidden">
         <img src="/titlebar.png" alt="" className="border-b " />
         <div className="flex flex-col relative ">
-            <img src="/galaria.png" alt="" className="object-cover w-full h-full" />      
-            <img src="/galaria2.png" alt="" className="object-cover w-full h-full" />
+            <img src="/gal.png" alt="" className="object-cover w-full h-full" />      
+            <img src="/gal2.png" alt="" className="object-cover w-full h-full" />
 
             <a href="" className="absolute bottom-4 right-4 text-xs bg-white px-2 shadow border w-max p-1  rounded-md flex items-center gap-1"><span>harvestgrove</span>
           <HiOutlineArrowUpRight/>
