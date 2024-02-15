6a27ed25dcb738190dbac6adaf7223564a569773
tohir babatunde <tohirbabs@gmail.com>
2024-02-15 17:22:10 +0100

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -71,14 +71,15 @@ const NavButton = ({link}) => {
     
     return (
         <div
-      {...handlers}
       style={{top: !navOpen ? "80vh":"30vh" }}
           className="absolute z-30 lg:static h-[80vh] w-screen transition-all backdrop-blur lg:backdrop-blur-none overflow-y-auto rounded-t-3xl lg:rounded-t-none flex flex-col gap-3  p-1 lg:w-64 lg:h-screen lg:bg-slate-50 bg-white/80  lg:border-r border">
         <div 
        className="flex justify-center  lg:hidden">
           <div className="w-16 h-1 rounded-full bg-slate-800 animate-bounce mt-2"></div>
         </div>
-        <div className="flex items-center gap-2">
+        <div
+              {...handlers}
+              className="flex items-center gap-2">
         <a href="/" className="flex flex-1 hover:bg-slate-200 p-1 rounded-lg items-center gap-2">
           <img src="/selfai.png" alt="" className="w-12 h-12 rounded-md bg-slate-500" />
           <div className="leading-tight">
