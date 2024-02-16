2c703c96d4f89ef30aec235e25a523d98d241bfe
tohir babatunde <tohirbabs@gmail.com>
2024-02-16 02:39:10 +0100

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ function Layout({children}) {
 
 
   return (
-    <div className="lg:p-3 p-1 bg-slate-900 font-mono text-sm relative flex lg:flex-row flex-col gap-3 w-screen overflow-hidden h-screen">
+    <div className="lg:p-3 p-1 bg-slate-900 font-mono text-sm relative flex lg:flex-row flex-col lg:gap-3 gap-1 w-screen overflow-hidden h-screen">
       <Nav/>
       <div className="bg-grid-[#80808012] bg-white rounded-2xl flex flex-col lg:p-6 lg:px-[8vw] pb-[6rem] flex-1 h-full overflow-y-auto">
         {children}
