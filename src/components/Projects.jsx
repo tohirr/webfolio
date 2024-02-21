15d7ba18db215ff35acb54102ebebba38164f44c
tohir babatunde <tohirbabs@gmail.com>
2024-02-21 09:37:35 +0100

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,23 +28,22 @@ const NavButton = ({link}) => {
 return (
     <div className=" max-w-2xl">
 
-        <div className="flex flex-col rounded-lg rounded-b-2xl  bg-white shadow overflow-hidden">
-        <img src="/titlebar.png" alt="" className="border-b " />
-        <div className="flex flex-col relative ">
-            <img src="/harvestgrove.png" alt="" className="object-cover w-full h-full" />      
-            {/* <img src="/sect2.png" alt="" className="object-cover w-full h-full" />      */}
-                   {/* <img src="/sect3.png" alt="" className="object-cover w-full h-full" /> */}
-            {/* <img src="/sect4.png" alt="" className="object-cover w-full h-full" /> */}
-            {/* <img src="/sect5.png" alt="" className="object-cover w-full h-full" /> */}
-      {/* <img src="/footer.png" alt="" className="object-cover w-full h-full" /> */}
+        <div className="flex items-center justify-center rounded-lg relative rounded-b-2xl py-16 px-10  bg-white shadow overflow-hidden">
+        <img src="/field.png" alt="" className="absolute top-0 h-full" />
+<div className="bg-white z-10 rounded-lg">
+<img src="/titlebar.png" alt="" className="border-b " />    
 
-            <a href="https://harvestgrover.vercel.app/" className="absolute bottom-4 right-4 text-xs bg-zinc-900 text-white shadow border border-zinc-400 w-max p-1  rounded-md flex items-center gap-1"><span>harvestgrove</span>
-            <div className="p-1  text-black bg-white rounded">
+<img src="/harvestgrove.png" alt="" className="object-cover w-full h-full" />      
+
+
+</div>
+
+            <a href="https://harvestgrover.vercel.app/" className="absolute bottom-4 right-4 text-xs bg-slate-200 text-black hover:bg-green-300 transition shadow border border-slate-50 w-max p-1  rounded-md flex items-center gap-1"><span>harvestgrove</span>
+            <div className="p-1  text-white bg-green-800 rounded">
             <HiOutlineArrowUpRight />
 
             </div>
-        </a>
-            </div></div>
+        </a></div>
             <div className="flex flex-col  gap-3 ">
             <p className="text-xs bg-red-200 m-3 shadow border w-max p-1 px-2 rounded-md ">pandawidgtes.&#128214;</p>
             <div className=" relative flex max-w-[20rem] lg:max-w-none justify-center gap-4 flex-wrap">
@@ -53,7 +52,7 @@ return (
               <Weather/>
             </div></div>  
             <div className="flex flex-col  rounded-lg rounded-b-2xl border-4 border-slate-600  shadow overflow-hidden">
-        <img src="/titlebard.png" alt="" celassName="border-b " />
+        {/* <img src="/titlebard.png" alt="" celassName="border-b " /> */}
         <div className="flex flex-col relative ">
             <img src="/gal.png" alt="" className="object-cover w-full h-full" />      
             <img src="/gal2.png" alt="" className="object-cover w-full h-full" />
